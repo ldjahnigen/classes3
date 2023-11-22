f4182f9b670daf489388c28e83026c8eef37eb2a
Louis <ldjahnigen@gmail.com>
2023-11-22 18:12:20 -0500

    added comments

--- main.cpp
+++ main.cpp
@@ -12,20 +12,24 @@
 
 
 int main() {
+  // temporary variables to store input data
   std::string manufacturer;
   int year;
   int doors;
   int towCapacity;
 
+  // get data for vehicle, use getline to account for spaces
   std::cout << "Vehicle\n";
   std::cout << "Enter the manufacturer: ";
   std::getline(std::cin, manufacturer);
   std::cout << "Enter the year: ";
   std::cin >> year;
 
+  // construct object and display info
   Vehicle v = Vehicle(manufacturer, year);
   v.displayInfo();
 
+  // get data for car, use ignore to clear input buffer
   std::cout << "\nCar\n";
   std::cout << "Enter the manufacturer: ";
   std::cin.ignore();
@@ -35,9 +39,11 @@ int main() {
   std::cout << "Enter the number of doors: ";
   std::cin >> doors;
 
+  // construct object and display info
   Car c = Car(manufacturer, year, doors);
   c.displayInfo();
 
+  // get data for truck
   std::cout << "\nTruck\n";
   std::cout << "Enter the manufacturer: ";
   std::cin.ignore();
@@ -47,6 +53,7 @@ int main() {
   std::cout << "Enter the tow capacity: ";
   std::cin >> towCapacity;
 
+  // construct object and display info
   Truck t = Truck(manufacturer, year, towCapacity);
   t.displayInfo();
 
